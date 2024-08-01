1b13ec3ed21012cb3f6290e82d65f01cdbc4da59
Col-R <2colewestin@gmail.com>
2024-08-01 13:51:49 -0500

    background colors for stats section

--- ui/components/home/HeroStats.tsx
+++ ui/components/home/HeroStats.tsx
@@ -1,21 +1,25 @@
 function HeroStats() {
     return (
-      <div className="flex flex-wrap justify-evenly gap-20 mb-4">
-        <div className="text-center">
-          <h2 className="text-5xl font-bold">$125m</h2>
-          <p className="text-gray-500">Revenue Gained</p>
+      <div className="flex flex-wrap w-full">
+        <div className="w-full md:w-1/2 bg-black text-white flex flex-row items-center justify-center">
+          <div className="text-center p-10">
+            <h2 className="text-5xl font-bold">$125m</h2>
+            <p className="text-gray-20">Revenue Gained</p>
+          </div>
+          <div className="text-center p-10">
+            <h2 className="text-5xl font-bold">200+</h2>
+            <p className="text-gray-200">Projects</p>
+          </div>
         </div>
-        <div className="text-center">
-          <h2 className="text-5xl font-bold">200+</h2>
-          <p className="text-gray-500">Projects</p>
-        </div>
-        <div className="text-center">
-          <h2 className="text-5xl font-bold">13</h2>
-          <p className="text-gray-500">Industry Awards</p>
-        </div>
-        <div className="text-center">
-          <h2 className="text-5xl font-bold">70+</h2>
-          <p className="text-gray-500">Years of Experience</p>
+        <div className="w-full md:w-1/2 bg-right-stat text-white flex flex-row items-center justify-center">
+          <div className="text-center p-10">
+            <h2 className="text-5xl font-bold">13</h2>
+            <p className="text-gray-200">Industry Awards</p>
+          </div>
+          <div className="text-center p-10">
+            <h2 className="text-5xl font-bold">70+</h2>
+            <p className="text-gray-200">Years of Experience</p>
+          </div>
         </div>
       </div>
     );
