ae107ae367b04cc64c1900a0a0a604938b515b43
Col-R <2colewestin@gmail.com>
2024-08-01 13:23:30 -0500

    navbar background transition

--- ui/components/home/HeroSection.tsx
+++ ui/components/home/HeroSection.tsx
@@ -2,6 +2,8 @@ import HeroStats from './HeroStats';
 import HeroQuote from './HeroQuote';
 import Title from './title';
 
+import TypedText from './TypedText';
+
 import Image from 'next/image'
 
 function HeroSection() {
@@ -18,9 +20,18 @@ function HeroSection() {
             className="z-0"
             />
 
-            <div className=" z-10 flex items-start justify-center h-full pt-10 text-left">
+            
+            <div className="absolute inset-0 bg-black opacity-40"></div>
+            <div className="container relative mx-auto z-10 flex flex-col align-center justify-center h-full">
               
-              <Title />
+              <h1 className="text-gray-200 text-6xl font-extrabold text-left mb-3" style={{ height: '130px' }}>
+                AECOM Viz Studio -
+                <TypedText />
+              </h1>
+                
+              <div>
+                <h2 className="text-gray-300 text-2xl max-w-2xl mt-12 mx-auto text-center">Welcome to our project showcase. We excel in bringing ideas to life through stunning visuals. Discover our work and get in touch with us.</h2>
+              </div>
               
             </div>
             {/* <div className="relative z-10 flex items-start justify-center h-full pt-20 text-center">
