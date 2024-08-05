efb133dcd4f021d6daf74ea812064563ac75c090
Col-R <2colewestin@gmail.com>
2024-08-05 15:05:03 -0500

    prepping for styling work tomorrow

--- ui/components/home/BusinessLines.tsx
+++ ui/components/home/BusinessLines.tsx
@@ -47,9 +47,9 @@ const BusinessLineCard: React.FC<BusinessLineCardProps> = ({ title }) => {
     ];
 
     return (
-        <section className="py-24 main-background">
+        <section className="py-16">
         <div className="container mx-auto">
-          <h2 className="text-white text-3xl font-bold text-center mb-8">Trusted Across Industries</h2>
+          <h2 className=" text-3xl font-bold text-center mb-8">Trusted Across Industries</h2>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
             {businessLinesData.map((line, index) => (
               <BusinessLineCard key={index} {...line} />
