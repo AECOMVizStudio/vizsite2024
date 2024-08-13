7fe2856b97885a7e7f5e2e821c2b2ca9e3f756a4
Col-R <2colewestin@gmail.com>
2024-08-13 10:16:51 -0500

    improved footer layout to stay at bottom of page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,27 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 
-import Header from '@/ui/header'
-import Footer from'@/ui/footer'
+import Header from '@/ui/header';
+import Footer from '@/ui/footer';
+import { Inter } from 'next/font/google';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
-export const metadata: Metadata = {
-  title: "AECOM Visualization Studio",
+export const metadata = {
+  title: 'AECOM Visualization Studio',
   description: "AECOM's highest rated visualization experts. We specialize in 3D modeling, realtime 3D, simulations, and more for business lines such as Water, Power, and Transportation",
 };
 
-export default function RootLayout({ 
-  children,}: {
+export default function RootLayout({
+  children,
+}: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
+    <html lang="en" className="h-full">
+      <body className={`${inter.className} h-full flex flex-col`}>
         <Header />
-        <main>{children}</main>
+        <main className="flex-grow">{children}</main>
         <Footer />
       </body>
     </html>
