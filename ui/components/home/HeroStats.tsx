714e5d5b5a416228809eb172cdcff52a906edb6b
Col-R <2colewestin@gmail.com>
2024-09-23 14:11:46 -0500

    better Hero stats section

--- ui/components/home/HeroStats.tsx
+++ ui/components/home/HeroStats.tsx
@@ -1,8 +1,9 @@
 function HeroStats() {
   const stats = [
-    { title: 'Revenue Gained', value: '$1bn' },
+    { title: 'Revenue Gained', value: '$1bn+' },
     { title: 'Projects', value: '2,000+' },
-    { title: 'Industry Awards', value: '5' },
+    { title: 'Clients Served', value: '500+' },
+    
     { title: 'Years of Experience', value: '100+' },
   ]
   
@@ -10,7 +11,7 @@ function HeroStats() {
     <div className="grid gap-4 md:grid-cols-4 grid-cols-2">
 
       {stats.map((stat) =>(
-        <div key={stat.title} className="text-center py-8 transition transform hover:rotate-2">
+        <div key={stat.title} className="text-center py-8 bg-white gridhover">
           <h2  className="text-4xl md:text-5xl font-bold">{stat.value}</h2>
           <p className="text-gray-700">{stat.title}</p>
         </div>
